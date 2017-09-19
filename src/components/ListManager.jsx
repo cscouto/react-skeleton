8d780ff27da689f83b703163b932bbc72befe650
TIAGO-LAPTOP\Tiago <cs.couto@outlook.com>
2017-09-18 23:16:31 -0300

    ajustes

--- src/components/ListManager.jsx
+++ src/components/ListManager.jsx
@@ -19,13 +19,19 @@ var ListManager = React.createClass({
     }, 
     render: function() {
         return (
-            <div>
-                <h3>{this.props.title}</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.onChange} value={this.state.newItemText} />
-                    <button>Add</button>
-                </form>
-                <List items={this.state.items} />
+            <div className="col-sm-4">
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3>{this.props.title}</h3>
+                    </div>
+                    <div className="panel-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <input onChange={this.onChange} value={this.state.newItemText} />
+                            <button>Add</button>
+                        </form>
+                        <List items={this.state.items} />
+                    </div>
+                </div>
             </div>
         );
     }
